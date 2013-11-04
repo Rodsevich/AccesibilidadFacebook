20d8243a682751360d319ce8167855b910e9b5f8
Likormack <chano_licha@hotmail.com>
2013-11-04 14:01:59 -0300

    comentarios en otrs paginas
    empezamos el caso de uso de ver los comentarios en otros lados. Hay q
    ver bien el tema de crear el link

---
+++ FacebookHiddenComentarios.js
@@ -0,0 +1,34 @@
+var metadata = { "author": "Lisandro Ronconi",
+    "name": "Oculta los comentarios de fecebook",
+    "description": "Este refactoring oculta los comentarios de los estados",
+    "id":"hiddencomentarios-Facebook-licha"};
+
+function getAccessibilityAugmenter(){
+    return new FacebookHiddenComentarios();
+};
+
+function FacebookHiddenComentarios() {
+
+};
+
+FacebookHiddenComentarios.prototype = new AbstractInstanceRefactoring();
+
+FacebookHiddenComentarios.prototype.setTargetURLs = function () {
+    this.addTargetURL(/https:\/\/www.facebook.com\//);
+};
+
+
+//override
+FacebookHiddenComentarios.prototype.adaptDocument = function (doc) {
+    if (doc.body.getAttribute("class") == "hasLeftCol home composerExpanded fbx gecko win Locale_es_ES") {
+        //esta en el muro de alguien logueado
+        this.abstract_refactoring.adaptDocument(doc);
+    }
+};
+
+
+
+FacebookHiddenComentarios.prototype.initialize = function (language) {
+    this.abstract_refactoring = new HiddenRedundantOperationRefactoring.HiddenRedundantOperationRefactoring("Facebook page");
+    this.abstract_refactoring.addClassAborrar(".uiUfi.UFIContainer");
+};
---
+++ FacebookMoveComentarios.js
@@ -0,0 +1,38 @@
+var metadata = { "author": "Andres, Lisandro",
+    "name": "Agrega el link",
+    "description": "Crea un link 'Ver estado con comentarios'",
+    "id":"movecomentarios-Facebook-lichaAndres"};
+
+function getAccessibilityAugmenter(){
+    return new FacebookMoveComentarios();
+};
+
+function FacebookMoveComentarios() {
+
+};
+
+FacebookMoveComentarios.prototype = new AbstractInstanceRefactoring();
+
+FacebookMoveComentarios.prototype.setTargetURLs = function () {
+    this.addTargetURL(/https:\/\/www.facebook.com\//);
+};
+
+
+//override
+FacebookMoveComentarios.prototype.adaptDocument = function (doc) {
+    if (doc.body.getAttribute("class") == "hasLeftCol home composerExpanded fbx gecko win Locale_es_ES") {
+        //esta en el muro de alguien logueado
+        this.abstract_refactoring.adaptDocument(doc);
+    }
+};
+
+
+FacebookMoveComentarios.prototype.initialize = function (language) {
+    var refactoring = new DistributeMenu.DistributeMenu("Facebook page");
+    refactoring.setItemXpath(".//span[@class='uiStreamFooter']");
+
+    var archive_operation = new DistributeMenu.DistributedOperation("Ver comentarios", refactoring);
+    archive_operation.setAction(".//span[@class='uiStreamFooter']/span[@class='uiStreamSource']/a");
+
+    this.abstract_refactoring = refactoring
+};
