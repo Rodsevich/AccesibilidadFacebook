fbd521492120dc5181a578254d5870b5e63ad535
Likormack <chano_licha@hotmail.com>
2013-11-21 10:55:23 -0300

    Oculta inicio y logo del facebook

---
+++ TPOO-CSWRFramework-1/Refactorings/instantiatedRefactorings/FacebookRemoveIndexLink.js
@@ -0,0 +1,23 @@
+var metadata = {"author":"Grupo facebook", "name":"Remover opcion de volver al inicio", "description":"Este refactoring quita la opcion inicio y el link del logo de facebok","id":"removeRedundant-index-todos"};
+
+function getAccessibilityAugmenter(){
+	return new FacebookRemoveRedundantOperations();
+};
+
+function FacebookRemoveRedundantOperations(){
+
+};
+
+FacebookRemoveRedundantOperations.prototype = new AbstractInstanceRefactoring();
+
+FacebookRemoveRedundantOperations.prototype.setTargetURLs = function(){
+	this.addTargetURL(/https:\/\/www.facebook.com\//);
+};
+
+FacebookRemoveRedundantOperations.prototype.initialize = function(language){
+		this.abstract_refactoring = new RemoveRedundantOperationRefactoring.RemoveRedundantOperationRefactoring("Facebook");
+		this.abstract_refactoring.addRedundantOperation("//*[@id='pageLogo']");
+		this.abstract_refactoring.addRedundantOperation("//*[@id='navHome']");
+		
+};
+
